e3f3a82e13dba473a0a7ef22fa7f6deac28b8b87
Magnus Eriksson <eriksson.mag@gmail.com>
2021-06-24 19:20:48 +0200

    Update build.rs

--- raylib-sys/build.rs
+++ raylib-sys/build.rs
@@ -62,7 +62,8 @@ fn build_with_cmake(src_path: &str) {
         .define("CMAKE_BUILD_TYPE", "Release")
         // turn off until this is fixed
         .define("SUPPORT_BUSY_WAIT_LOOP", "OFF")
-        .define("STATIC", "TRUE");
+        .define("STATIC", "TRUE")
+        .define("CMAKE_C_FLAGS", "-DDEFAULT_GRAPHIC_DEVICE_DRM=\"/dev/dri/card0\"");
 
     /*    match platform {
         Platform::Desktop => conf.define("PLATFORM", "Desktop"),
@@ -72,7 +73,7 @@ fn build_with_cmake(src_path: &str) {
 */
     conf.define("PLATFORM", "DRM");
     conf.define("DEFAULT_GRAPHIC_DEVICE_DRM", "\"/dev/dri/card0\"");
-    conf.define("CMAKE_C_FLAGS", "-DDEFAULT_GRAPHIC_DEVICE_DRM=\"/dev/dri/card0\"");
+//    conf.define("CMAKE_C_FLAGS", "-DDEFAULT_GRAPHIC_DEVICE_DRM=\"/dev/dri/card0\"");
     let dst = conf.build();
     let dst_lib = join_cmake_lib_directory(dst);
     // on windows copy the static library to the proper file name
